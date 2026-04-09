267d220f6de05ce636f8f45a0a95bebe8c43bb8f
frank1360 <frankpaez.07@gmail.com>
2026-04-09 16:47:57 -0400

    fix de error

--- src/components/dashboard/NotificationBell.tsx
+++ src/components/dashboard/NotificationBell.tsx
@@ -26,6 +26,7 @@ export function NotificationBell() {
 
     // Fetch inicial al montar
     useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
         fetchMetrics();
     }, [fetchMetrics]);
 
